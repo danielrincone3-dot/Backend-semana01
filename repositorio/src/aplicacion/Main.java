11ad09065fb5032472b1a28c5bfb79d69ac1ab11
daniel <danielrincone3@gmail.com>
2026-02-07 14:20:11 -0500

    repaso java

--- repositorio/src/aplicacion/Main.java
+++ repositorio/src/aplicacion/Main.java
@@ -1,8 +1,24 @@
 package aplicacion;
 
-public class Main {
+public class main {
     public static void Main(String[] arge) {
         System.out.println("hello work");
+        String name;
+        String lastName;
+
+        name = "daniel";
+        lastName = "herrera";
+        int year = Integer.parseInt("24");
+        boolean male = false;
+
+        System.out.println("My name is "+ name + lastName);
+        System.out.println("I am " + year + "year old");
+        System.out.println("I am male?" + male);
+
+        for (int i = 1; 1 <= 10; i++) {
+            System.out.println("Interration; " + 1);
+        }
+
     }
 
 
