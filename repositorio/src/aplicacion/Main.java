623d1677e72eb2dbb85ecee8858811dc5ba9fbcc
daniel <danielrincone3@gmail.com>
2026-02-14 14:01:05 -0500

    repositorio de semana03

--- repositorio/src/aplicacion/Main.java
+++ repositorio/src/aplicacion/Main.java
@@ -5,26 +5,26 @@ import java.util.List;
 
 public class Main {
     final static int CURRENT_YEAR = 2026;
+    static List<Integer> agesStudentList = new ArrayList<>();
 
     public static void main(String[] args) {
-        final String GREETING_MESSAGE = "saludo estudiante de java";
+        final String GREETING_MESSAGE = "Saludo estudiante de Java";
+        final String LIST_HEADER = "Lista de estudiantes:";
 
         String[] namesStudents = {"Emanuel", "Matius", "Ronaldo", "Messi", "Carlos"};
         int[] birthYears = {2000, 2001, 2007, 2009, 1995};
 
-
-        List<Integer> agesStudents = new ArrayList<>();
+        List<Integer> agesStudentsLocal;
 
         showMessage(GREETING_MESSAGE);
+        showMessage(LIST_HEADER);
 
+        agesStudentsLocal = fillAgesList(birthYears);
 
-        for (int i = 0; i < birthYears.length; i++) {
-            int age = calculateAge(birthYears[i]);
-            agesStudents.add(age);
-            System.out.println(namesStudents[i] + " tiene " + age + " años.");
+        for (int i = 0; i < namesStudents.length; i++) {
+            System.out.println(namesStudents[i] + " tiene " + agesStudentsLocal.get(i) + " años.");
         }
     }
-
 
     static void showMessage(String message) {
         System.out.println(message);
@@ -32,5 +32,13 @@ public class Main {
 
     static int calculateAge(int birthYear) {
         return CURRENT_YEAR - birthYear;
+    }
+
+    static List<Integer> fillAgesList(int[] birthYears) {
+        for (int year : birthYears) {
+            int age = calculateAge(year);
+            agesStudentList.add(age);
+        }
+        return agesStudentList;
     }
 }
